4c33759319969d0421b32bea219662ebaa4578f8
Harshal Patil <harshalpatil2012@users.noreply.github.com>
2024-04-22 18:45:20 +0530

    added loggers

--- src/main/java/com/example/bookapp/BookApplication.java
+++ src/main/java/com/example/bookapp/BookApplication.java
@@ -1,6 +1,8 @@
 
 package com.example.bookapp;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
+    }
+
+
+    @Value("${server.compression.enabled}")
+    private boolean compressionEnabled;
+
+    @Value("${server.compression.min-response-size}")
+    private int minResponseSize;
+
+
+    @PostConstruct
+    public void logCompressionSettings() {
+        System.out.println("Compression Enabled: " + compressionEnabled);
+        System.out.println("Minimum Response Size for Compression: " + minResponseSize);
     }
 }
